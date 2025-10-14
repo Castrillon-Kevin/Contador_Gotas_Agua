97964695b84d48b8d125a6586f2bd2bf4a809491
sebastiangama <gamasebastian422@gmail.com>
2025-10-14 09:56:47 -0500

    actualizacion index

--- javascript/administracion.js
+++ javascript/administracion.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const rol = sessionStorage.getItem("rol");
-    if (rol !== "admin") {
+    if (rol !== "1") {
         alert("Acceso denegado. No tienes permisos de administrador.");
         window.location.href = "../index.html";
     }
